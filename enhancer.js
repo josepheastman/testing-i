532149f3438b39ddba2de745481f46c5a2fa8331
josepheastman <josephryan96@gmail.com>
2019-01-28 16:07:09 -0800

    Fix

--- enhancer.js
+++ enhancer.js
@@ -7,15 +7,15 @@ module.exports = {
 function success(item) {
   if (item.enhancement > 0 && item.enhancement <= 15) {
     item.enhancement += 1;
-  } else if ((item.enhancement = 16)) {
+  } else if (item.enhancement === 16) {
     item.enhancement = "PRI";
-  } else if ((item.enhancement = 17)) {
+  } else if (item.enhancement === 17) {
     item.enhancement = "DUO";
-  } else if ((item.enhancement = 18)) {
+  } else if (item.enhancement === 18) {
     item.enhancement = "TRI";
-  } else if ((item.enhancement = 19)) {
+  } else if (item.enhancement === 19) {
     item.enhancement = "TET";
-  } else if ((item.enhancement = 20)) {
+  } else if (item.enhancement === 20) {
     item.enhancement = "PEN";
   }
   return item;
